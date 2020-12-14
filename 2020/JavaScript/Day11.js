fc8b3facaa813cecf37af00f97f69bd05b911664
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-12-14 20:20:35 +0100

    feat: preperations for day 11

--- 2020/JavaScript/Day11.js
+++ 2020/JavaScript/Day11.js
@@ -1,66 +1,30 @@
-import fetchData from "/Fetch.js";
-fetchData("./Inputs/11-data.txt", day11);
+import {
+  resolve,
+  dirname
+} from "path";
+import {
+  readFileSync
+} from "fs";
+import {
+  fileURLToPath
+} from 'url';
 
-function day11(input) {
-  const rows = input.split(/\n/);
-  let seats = [];
-  const empty = "L";
-  const floor = ".";
-  const occupied = "#";
-  
-  rows.forEach(row => {
-    seats.push(row.split(""));
-  });
+const __filename = fileURLToPath(
+  import.meta.url);
+const __dirname = dirname(__filename);
 
-  let prev = seats;
-  let next = [];
+const filePath = resolve(__dirname, "../Inputs/11-data.txt");
+const data = readFileSync(filePath, "utf-8");
 
-  const boolArrs = arrayEquals(prev, next);
+let seats = [];
+const rows = data
+  .split("\n")
+  .forEach(row => seats.push(row.split("")))
 
-  console.log(mapArr(prev));
+const empty = "L";
+const floor = ".";
+const occupied = "#";
 
-  if (boolArrs === true) {
-    return console.log("The same: " + next);
-  }
-}
-
-function arrayEquals(a, b) {
-  return Array.isArray(a) &&
-    Array.isArray(b) &&
-    a.length === b.length &&
-    a.every((val, index) => val === b[index]);
-}
-
-function mapArr(arr) {
-  arr.forEach((row, i, array) => {
-    row.map((seat, j) => {
-      const curRow = i;
-      const curSeat = j;
-
-      const topRow = i-1;
-      const bottomRow = i+1;
-
-      const rows = [curRow, topRow, bottomRow];
-
-      
-      const leftSeat = j-1
-      const rightSeat = j+1;
-
-      const seats = [curSeat, leftSeat, rightSeat]
-
-      let adjacent = [];
-      rows.forEach(el => {
-        seats.forEach(ol => {
-          if (array[el][ol] !== undefined) {
-            adjacent.push(array[el][ol])
-          }
-        })
-      })
-    
-      console.log(adjacent)
-      if (seat === "L") {
-        
-      }
-    })
-  })
-}
+/**
+ * Part One
+ */
