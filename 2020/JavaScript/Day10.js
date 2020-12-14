63f642cb79162fa65d5400907a5d0f03a774703f
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-12-14 18:54:19 +0100

    feat: write part 1 differently

--- 2020/JavaScript/Day10.js
+++ 2020/JavaScript/Day10.js
@@ -1,40 +1,50 @@
-import fetchData from "/Fetch.js";
-fetchData("./Inputs/10-data.txt", day10);
+import {
+  resolve,
+  dirname
+} from "path";
+import {
+  readFileSync
+} from "fs";
+import {
+  fileURLToPath
+} from 'url';
 
-function day10(input) {
-  const data = input.split(/\n/).map(n => Number(n));
-  const sortedData = [0, ...data.sort((a, b) => a - b)]; // Add the socket of 0 to the front and spread the data array into the new array, sorted ascending
-  const lastNum = sortedData[sortedData.length-1]
-  sortedData.push(lastNum+3); // add the device of n+3 at the end of the array
+const __filename = fileURLToPath(
+  import.meta.url);
+const __dirname = dirname(__filename);
 
-  console.log("Part One: " + partOne(sortedData))
-  console.log("Part Two: " + partTwo(sortedData));
-}
+const filePath = resolve(__dirname, "../Inputs/10-data.txt");
+const data = readFileSync(filePath).toString();
 
-function partOne(arr) {
-  let oneJolt = 0;
-  let twoJolts = 0;
-  let threeJolts = 0;
-
-  for (let n = 0; n < arr.length; n++) {
-    const sub = arr[n+1] - arr[n];
-    switch (sub) {
-      case 1:
-        oneJolt++;
-        break;
-      case 2:
-        twoJolts++;
-        break;
-      case 3:
-        threeJolts++;
-        break;
+const adapters = data.split("\n")
+  .map(joltage => Number(joltage))
+  .sort((a, b) => a - b)
+  .map(joltage => {
+    return {
+      minJoltage: joltage,
+      maxJoltage: joltage + 3
     }
-  }
-  return oneJolt * threeJolts;
-}
+  });
+const lastAdapter = adapters[adapters.length - 1];
+adapters.push({
+  minJoltage: lastAdapter.minJoltage + 3,
+  maxJoltage: lastAdapter.maxJoltage + 3
+});
 
-function partTwo(arr){
-  
-}
+/**
+ * Part One
+ */
+const joltageCount = [];
 
-console.log("hello")
+adapters
+  .slice(0, adapters.length - 1)
+  .forEach((adapter, index) => {
+    const diff = adapters[index + 1].minJoltage - adapter.minJoltage;
+    if (!joltageCount[diff]) {
+      joltageCount[diff] = 0;
+    }
+    joltageCount[diff]++;
+  });
+
+const partOne = joltageCount[1] * joltageCount[3];
+console.log(partOne)
