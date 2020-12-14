004c6cb8aaaa2a457355d2fe6c89175ec4f71888
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-12-14 21:49:25 +0100

    feat: solution for part 1

--- 2020/JavaScript/Day11.js
+++ 2020/JavaScript/Day11.js
@@ -16,10 +16,10 @@ const __dirname = dirname(__filename);
 const filePath = resolve(__dirname, "../Inputs/11-data.txt");
 const data = readFileSync(filePath, "utf-8");
 
-let seats = [];
-const rows = data
+const seats = data
   .split("\n")
-  .forEach(row => seats.push(row.split("")))
+  .filter(line => line)
+  .map(row => row.split(""))
 
 const empty = "L";
 const floor = ".";
@@ -28,3 +28,89 @@ const occupied = "#";
 /**
  * Part One
  */
+
+function iterate(original) {
+  let changedSeat = false;
+  const next = original.map((line, row) => {
+    return line.map((seat, col) => {
+      const newState = applyRules(original, row, col);
+      if (original[row][col] !== newState) {
+        changedSeat = true;
+      }
+      return newState;
+    })
+  });
+
+  return {
+    "state": next,
+    changedSeat
+  };
+}
+
+function applyRules(state, row, col) {
+  if (state[row][col] === floor) {
+    return floor;
+  }
+
+  const adjacentSeats = countAdjacentSeats(state, row, col);
+
+  if (adjacentSeats === 0 && state[row][col] === empty) {
+    return occupied;
+  } else if (adjacentSeats >= 4 && state[row][col] === occupied) {
+    return empty;
+  }
+
+  return state[row][col];
+}
+
+function countAdjacentSeats(state, row, col) {
+  return [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, -1],
+      [0, 1],
+      [1, -1],
+      [1, 0],
+      [1, 1]
+    ]
+    .map(([drow, dcol]) => {
+      return [
+        row + drow,
+        col + dcol
+      ];
+    })
+    .filter(([nrow, ncol]) => {
+      return (nrow >= 0 && nrow < state.length) &&
+        (ncol >= 0 && ncol < state[nrow].length);
+    })
+    .filter(([nrow, ncol]) => {
+      return state[nrow][ncol] === occupied;
+    })
+    .length;
+}
+
+function chaos(original) {
+  let changedSeat = true;
+  let state = original;
+  let count = 1;
+  while (changedSeat) {
+    ({
+      state,
+      changedSeat
+    } = iterate(state));
+    count++;
+  }
+
+  let totalCount = 0;
+  const total = state.map(row => {
+      return row.filter(seat => seat === occupied);
+    })
+    .forEach(line => {
+      totalCount += line.length;
+    })
+
+  console.log(totalCount)
+}
+
+chaos(seats);
