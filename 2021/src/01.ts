f082c873126c6eeccfeccd93db6bb56f6c097d92
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-12-01 16:54:33 +0100

    ♻️ refactor code to use one reduce function

--- 2021/src/01.ts
+++ 2021/src/01.ts
@@ -1,8 +1,11 @@
 import { getData } from "./getData";
 
 const DATA = getData(1);
-
 const data = DATA.split("\r\n").map((item) => Number(item));
+
+/**
+ * Original Attempt
+ */
 
 const measurements = {
   increase: 0,
@@ -41,6 +44,7 @@ const slidingWindows = data.reduce((acc: Array<number>, first, index, arr) => {
   return acc;
 }, []);
 
+prev = null;
 slidingWindows.forEach((item) => {
   if (prev === null) {
     prev = item;
@@ -58,3 +62,32 @@ slidingWindows.forEach((item) => {
 });
 
 console.log(measurements);
+
+/**
+ * Refactored Attempt
+ */
+
+const results = data.reduce(
+  (accumulation: Results, current, index, array): Results => {
+    const prev = array[index - 1];
+    const next = array[index + 1];
+    const prevWindow = prev + current + next;
+    const currentWindow = current + next + array[index + 2];
+
+    const updated = { ...accumulation };
+
+    if (current > prev) {
+      updated.increase++;
+    }
+    if (Boolean(currentWindow) && currentWindow > prevWindow) {
+      updated.windowIncrease++;
+    }
+
+    return updated;
+  },
+  { increase: 0, windowIncrease: 0 }
+);
+
+type Results = { increase: number; windowIncrease: number };
+
+console.log(results);
