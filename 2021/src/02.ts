2c06b51ccd1a8f80f23cd7bc9f92282c548ba6d8
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-12-02 09:12:55 +0100

    :christmas_tree: solve puzzle 2 of day 2

--- 2021/src/02.ts
+++ 2021/src/02.ts
@@ -3,6 +3,12 @@ import { getData } from "./getData";
 const DATA = getData(2);
 const data = DATA.split("\r\n");
 
+/**
+ * Original Attempt
+ */
+
+// Part 1
+
 const movements: Movements = {
   x: 0,
   y: 0,
@@ -36,4 +42,54 @@ type Movements = {
   y: number;
   multiplication: number;
 };
-console.log(results.multiplication);
+
+console.log(results);
+
+// Part 2
+
+const aiming: Aiming = {
+  x: 0,
+  y: 0,
+  get multiplication() {
+    return this.x * this.y;
+  },
+  depth: 0,
+  aim: 0,
+  get position() {
+    return this.depth * this.x;
+  }
+};
+
+type Aiming = {
+  x: number;
+  y: number;
+  multiplication: number;
+  depth: number;
+  aim: number;
+  position: number;
+};
+
+const aimResults = data.reduce((accumulator: Aiming, current) => {
+  const [direction, steps] = current.split(" ");
+
+  switch (direction) {
+    case "up":
+      accumulator.y -= parseInt(steps);
+      accumulator.aim -= parseInt(steps);
+      break;
+    case "down":
+      accumulator.y += parseInt(steps);
+      accumulator.aim += parseInt(steps);
+      break;
+    case "forward":
+      accumulator.x += parseInt(steps);
+      accumulator.depth += (parseInt(steps) * accumulator.aim);
+      break;
+    default:
+      break;
+  }
+
+  return accumulator;
+}, aiming);
+
+console.log(aimResults.position);
