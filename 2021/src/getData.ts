6e901a14123bfff173ebb3f30808c9d6ba5449be
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-12-02 08:41:18 +0100

    🔇 remove console log

--- 2021/src/getData.ts
+++ 2021/src/getData.ts
@@ -1,9 +1,6 @@
 import fs from "fs";
 import path from "path";
 
-const day = "01";
-console.log(path.join(path.dirname(__dirname), "inputs", day + ".txt"));
-
 export const getData = (dayNumber: number) => {
   const day = dayNumber.toString().padStart(2, "0");
   const filePath = path.join(path.dirname(__dirname), "inputs", day + ".txt");
