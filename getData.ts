ce7096ea5c52d6ae7b7f0d902e74b014e9cc1600
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-12-02 20:35:03 +0100

    🚚 move getData to root

--- getData.ts
+++ getData.ts
@@ -1,9 +1,9 @@
 import fs from "fs";
 import path from "path";
 
-export const getData = (dayNumber: number) => {
+export const getData = (year: number, dayNumber: number) => {
   const day = dayNumber.toString().padStart(2, "0");
-  const filePath = path.join(path.dirname(__dirname), "inputs", day + ".txt");
+  const filePath = path.join(year.toString(), "inputs", day + ".txt");
   const data = fs.readFileSync(filePath, "utf8");
   return data;
 };
