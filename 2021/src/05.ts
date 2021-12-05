a128ba525d5c9f01a3ac8db3647c9f454852c492
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-12-05 09:53:02 +0100

    💡 add comment for original attempt

--- 2021/src/05.ts
+++ 2021/src/05.ts
@@ -2,16 +2,9 @@ import { getData } from "../../getData";
 const DATA = getData(2021, 5);
 const data = DATA.split(/\r\n/);
 
-const testData = `0,9 -> 5,9
-8,0 -> 0,8
-9,4 -> 3,4
-2,2 -> 2,1
-7,0 -> 7,4
-6,4 -> 2,0
-0,9 -> 2,9
-3,4 -> 1,4
-0,0 -> 8,8
-5,5 -> 8,2`.split(/\n/);
+/**
+ * Original Attempt
+ */
 
 function calculateCrossingVents(input: string[]): number {
   const coords = input.map((line) => {
