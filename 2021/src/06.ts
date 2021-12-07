ef468367a53c831436d14961c7a0d5897a345ad5
Wiktoria Mielcarek <w.mielcarek@braweria.de>
2021-12-07 17:31:04 +0100

    :christmas_tree: solve part 1 day 6 2021

--- 2021/src/06.ts
+++ 2021/src/06.ts
@@ -3,38 +3,28 @@ const DATA = getData(2021, 6);
 const data = DATA.split(",").map(Number);
 const test = [3,4,3,1,2];
 
-let days = 80;
+/**
+ * 
+ * Original Attempt
+ * Part 1
+ */
 
-function startPopulation(group: number[], days: number) {
-    let fishes = [...group];
-    let amountOfNewFishes = 0;
-
-    console.log(group)
-
-    while ( days !== -1 ) {
-        for ( let i = 0; i < fishes.length; i++ ) {
-            const fish = fishes[i];
-            if ( fish - 1 === -1 ) {
-                fishes[i] = 6;
-                console.log("reborn", fish, i, fishes[i])
-            } else if (fish === 6){
-                console.log("born", fish, i, fishes[i])
-                amountOfNewFishes++;
-            } else {
-                fishes[i] = fish - 1;
-            }
-        }
-        days--;
-        
-        for (let i = 0; i <= amountOfNewFishes; i++) {
-            fishes.push(8);
-        }
-        amountOfNewFishes = 0;
-        console.log(fishes);
+function populateFishes(fishes: number[], days: number): number {
+    if (days === 0) {
+        return fishes.length;
     }
-
-    return fishes.length;
+const update = [];
+    for (let i = 0; i < fishes.length; i++) {
+        const fish = fishes[i];
+        const updatedFish = fish - 1;
+        if (updatedFish === -1) {
+            update.push(6);
+            update.push(8);
+        } else {
+            update.push(updatedFish);
+        }
+    }
+    return populateFishes(update, days - 1);
 }
 
-// console.log(startPopulation(data, days));
-console.log(startPopulation(test, 1));
+console.log(populateFishes(data, 80));
