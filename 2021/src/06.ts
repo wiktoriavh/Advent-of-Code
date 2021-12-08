40f9843307500f88e51dc22359d04c4b0a96de1b
Wiktoria Mielcarek <w.mielcarek@braweria.de>
2021-12-08 17:32:25 +0100

    WIP: part 1 day 6 2021

--- 2021/src/06.ts
+++ 2021/src/06.ts
@@ -27,9 +27,13 @@ const update = [];
     return populateFishes(update, days - 1);
 }
 
-console.log(populateFishes(data, 80));
+// console.log(populateFishes(data, 80));
 
-console.log(populateFishes(test, 256));
+/**
+ * Part 2
+ */
+
+const expectedTest2 = 26984457539;
 
 const fishObject = {
     0: 0,
@@ -42,3 +46,54 @@ const fishObject = {
     7: 0,
     8: 0,
 }
+
+function createObject(fishes: number []): {[key:string]:number} {
+    const obj: {[key:string]:number} = {...fishObject};
+    for (let i = 0; i < fishes.length; i++) {
+        const fish = fishes[i];
+        if (!obj[fish]) {
+            obj[fish] = 1;
+        } else {
+            obj[fish]++;
+        }
+    }
+    return obj;
+}
+
+const fishTimer = createObject(data);
+const testTimer = createObject(test);
+
+function growFishPopulation(fishes: {[key:string]:number}, days: number): number {
+ 
+    console.log(fishes);
+
+    let countdown = days;
+    let update: {[key:string]:number} = {...fishes};
+
+    while (countdown > 0) {
+        const group = {...update}
+        for (let key in group) {
+            if (Number(key) - 1 === -1) {
+                group["6"] = group["6"] ? group["6"] + update[key] + 1 : update[key] + 1;
+                group["8"] = group["8"] ? group["8"] + 1 : 1;
+            } else {
+                group[Number(key) - 1] = group[Number(key) - 1] ? group[Number(key) - 1] + update[key] : update[key];
+            }
+
+        }
+        update = {...group};
+        countdown--;
+    }
+    let count = 0;
+    for (let key in update) {
+        count += update[key];
+    }
+
+console.log(count);
+}
+
+
+const solutionTest2 = growFishPopulation(testTimer, 256);
+console.log(solutionTest2, expectedTest2);
+
+
