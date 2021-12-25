8e95264b8aba751189acc632cc144d3afd5869d6
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-12-25 09:46:12 +0100

    :christmas_tree: Solve part 2 day 7 2021

--- 2021/src/07.ts
+++ 2021/src/07.ts
@@ -1,10 +1,10 @@
 import { getData } from "../../getData";
 const DATA = getData(2021, 7);
 const data = DATA.split(",").map(Number);
-const example = [16,1,2,0,4,2,7,1,2,14];
+const example = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
 const exampleSolution = 37;
 
-type Crabs = number[]
+type Crabs = number[];
 
 /**
  * Original Attempt
@@ -12,19 +12,19 @@ type Crabs = number[]
  */
 
 function calculateCrabBeamFuel(crabs: Crabs) {
-    const sorted = crabs.sort((a, b) => a - b);
-    const pointer = Math.floor((sorted.length - 1) / 2);
-    const median = sorted[pointer];
-    
-    const fuel = crabs.reduce((acc, curr) => {
-         return acc += Math.abs(curr - median);
-    }, 0);
+  const sorted = crabs.sort((a, b) => a - b);
+  const pointer = Math.floor((sorted.length - 1) / 2);
+  const median = sorted[pointer];
 
-    return fuel;
+  const fuel = crabs.reduce((acc, curr) => {
+    return (acc += Math.abs(curr - median));
+  }, 0);
+
+  return fuel;
 }
 
 const beamFuel = calculateCrabBeamFuel(data);
-console.log(beamFuel);
+console.log("Part 1", beamFuel);
 
 /**
  * Part 2
@@ -33,10 +33,23 @@ console.log(beamFuel);
 const exampleSolution2 = 168;
 
 function crabEngineering(crabs: Crabs) {
-    const sorted = crabs.sort((a, b) => a - b);
-    const pointer = Math.floor((sorted.length - 1) / 2);
-    const median = sorted[pointer];
-    
+  let minFuel = Infinity;
+  const sorted = crabs.sort((a, b) => a - b);
 
-    return fuel;
+  for (let i = 0; i <= sorted[sorted.length - 1]; i++) {
+    let fuel = sorted.reduce((acc, curr) => {
+      return acc + calculateFuel(Math.abs(curr - i));
+    }, 0);
+    if (fuel < minFuel) {
+      minFuel = fuel;
+    }
+  }
+  return minFuel;
 }
+
+function calculateFuel(steps: number) {
+  return (steps * (steps + 1)) / 2;
+}
+
+const engineeredFuel = crabEngineering(data);
+console.log("Part 2", engineeredFuel);
